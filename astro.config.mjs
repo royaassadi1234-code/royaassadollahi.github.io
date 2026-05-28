15aecdb5cff2fafa0ec9b042c8ccf61eb778e747
Your Name <your.email@example.com>
2026-05-28 04:38:14 +0200

    Update config for GitHub Pages user repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://royaassadi1234-code.github.io',
-  base: '/b',
+  site: 'https://royaassadollahi.github.io',
 });
