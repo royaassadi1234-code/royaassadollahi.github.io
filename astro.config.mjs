be233fe18d2f497511885b6ede3de51809f1e71c
Your Name <your.email@example.com>
2026-05-28 13:55:41 +0200

    Update config for project repository deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://royaassadollahi.github.io',
+  site: 'https://royaassadi1234-code.github.io/royairanist.github.io',
+  base: '/royairanist.github.io/',
 });
