c1bfe4956152b28458574202a5418daa584d5ea4
Your Name <your.email@example.com>
2026-06-03 00:09:38 +0200

    corrections

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://royaassadi1234-code.github.io/royairanist.github.io',
-  base: '/royairanist.github.io/',
+  site: 'https://royaassadi1234-code.github.io',
+  base: '/',
 });
